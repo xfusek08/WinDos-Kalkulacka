c474b9fe3a2343d20dac61d9a724b058dd6a9a60
Petr Fusek <Petr Fusek>
2017-03-21 15:30:35 +0100

    Vytvoření testovací třidy pro matematickou knihovnu
    Vytváří kostru CalcMath
    Obsahuje testy metod Ådd a Subtract

---
+++ src/CalculatorProject/MathLib/CalcMath.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MathLib
+{
+  /// <summary>
+  /// Matematická knihovna ... doplnit komentáře
+  /// </summary>
+  public class CalcMath
+  {
+    // Sečte hodnoty 'a' a 'b'
+    public double Add(double a, double b)
+    {
+      return 0;
+    }
+
+    // Odečte hodnoty 'a' a 'b'
+    public double Subtract(double a, double b)
+    {
+      return 0;
+    }
+
+    // Vydělí hodnoty 'a' a 'b'
+    public double Divide(double a, double b)
+    {
+      return 0;
+    }
+
+    // Vynásobí hodnoty 'a' a 'b'
+    public double Multipy(double a, double b)
+    {
+      return 0;
+    }
+
+    // Umocní 'x' na 'y'
+    public double Pow(double x, double y)
+    {
+      return 0;
+    }
+
+    // Umocní 'x' na 'y'
+    public double Root(double a, double x)
+    {
+      return 0;
+    }
+
+    // Vypočte faktorial z 'x'
+    public long Fact(int x)
+    {
+      return 0;
+    }
+
+    // Najde zbytek po dělení 'a' a 'b'
+    public double Modulo(double a, double b)
+    {
+      return 0;
+    }
+
+    // Vypočte logaritmus z 'x' o základu 10 
+    public double Log(double x)
+    {
+      return 0;
+    }
+  }
+}
---
+++ src/CalculatorProject/ProjectTesting/MathLibTest.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MathLib;
+
+namespace ProjectTesting
+{
+  /// <summary>
+  /// Testování funkcí matematické knihovny
+  /// </summary>
+  [TestClass]
+  public class MathLibTest
+  {
+
+    public CalcMath mathclass;
+
+    public MathLibTest()
+    {
+      mathclass = new CalcMath();
+    }
+
+    [TestMethod]
+    public void TestAdd()
+    {
+      Assert.AreEqual(0, mathclass.Add(0, 0));
+      Assert.AreEqual(-1, mathclass.Add(1, -2));
+      Assert.AreEqual(1, mathclass.Add(0.5, 0.5));
+      Assert.AreEqual(double.PositiveInfinity, mathclass.Add(double.PositiveInfinity, 5));
+      Assert.AreEqual(double.NegativeInfinity, mathclass.Add(double.NegativeInfinity, 5));
+      Assert.IsTrue(double.IsNaN(mathclass.Add(double.NaN, 5)));
+      Assert.IsTrue(double.IsNaN(mathclass.Add(double.PositiveInfinity, double.NegativeInfinity)));
+    }
+
+    [TestMethod]
+    public void TestSubtract()
+    {
+      Assert.AreEqual(0, mathclass.Subtract(0, 0));
+      Assert.AreEqual(3, mathclass.Subtract(1, -2));
+      Assert.AreEqual(-5, mathclass.Subtract(3, 8));
+      Assert.AreEqual(0, mathclass.Subtract(0.5, 0.5));
+      Assert.AreEqual(-1.5, mathclass.Subtract(0.5, 2));
+      Assert.AreEqual(double.PositiveInfinity, mathclass.Subtract(double.PositiveInfinity, 5));
+      Assert.AreEqual(double.NegativeInfinity, mathclass.Subtract(double.NegativeInfinity, 5));
+      Assert.AreEqual(double.PositiveInfinity, mathclass.Subtract(0, double.NegativeInfinity));
+      Assert.AreEqual(double.NegativeInfinity, mathclass.Subtract(0, double.PositiveInfinity));
+      Assert.IsTrue(double.IsNaN(mathclass.Subtract(double.NaN, 5)));
+      Assert.IsTrue(double.IsNaN(mathclass.Subtract(double.PositiveInfinity, double.NegativeInfinity)));
+    }
+  }
+}
