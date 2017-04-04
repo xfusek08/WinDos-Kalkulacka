571c5177bc4fba1c65424429f0c623142a674daf
Petr Fusek <xfusek08@stud.fit.vutbr.cz>
2017-04-04 22:02:52 +0200

    Rozpracovaná funkce evaluace, přidána aplikace na interaktivní zkoušení vyhodnocování výrazů.

---
+++ src/CalculatorProject/CalcUnitTester/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CalculatorUnit;
+
+namespace CalcUnitParser
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      do
+      {
+        Console.Write("Expresion: ");
+        string expr = Console.ReadLine();
+
+        Calculation calculation = new Calculation(expr);
+        Console.WriteLine("Result: {0}", calculation.Value);
+      } while (true);
+    }
+  }
+}
