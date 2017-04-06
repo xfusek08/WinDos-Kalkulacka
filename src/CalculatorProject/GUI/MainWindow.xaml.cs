d2482a6469fe67ac1220ed3e34694081955b82be
Radim Blaha <xblaha28@stud.fit.vutbr.cz>
2017-04-06 17:16:16 +0200

    Indexy mocniny a odmocniny
    Byly přidány indexy u tlačítek mocniny a odmocniny, tak aby se správně vykreslovali i při změně okna.

--- src/CalculatorProject/GUI/MainWindow.xaml.cs
+++ src/CalculatorProject/GUI/MainWindow.xaml.cs
@@ -1,4 +1,18 @@
-﻿using System;
+﻿/*******************************************************************
+* Název projektu: GUI
+* Balíček: GUI
+* Soubor: MainWindow.xaml.cs
+* Datum: 05.04.2017
+* Autor: Radim Blaha
+* Naposledy upravil: Radim Blaha
+* Datum poslední změny: 06.04.2017
+*
+* Popis: Třída, která ovládá grafické prvky hlavního okna aplikace a
+*        reaguje na příchozí uživatelské události.
+*
+*****************************************************************/
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -23,6 +37,49 @@ namespace GUI
     public MainWindow()
     {
       InitializeComponent();
+    }
+
+
+    //===== Eventy spoštějící se při najetí a opuštění na tlačítka odmocniny =====
+    private void grdRootBtn_MouseEnter(object sender, MouseEventArgs e)
+    {
+      btnRoot.Background = new SolidColorBrush(Color.FromArgb(0xFF, 0x5C, 0x6b, 0xC0));
+    }
+
+    private void grdRootBtn_MouseLeave(object sender, MouseEventArgs e)
+    {
+      btnRoot.Background = new SolidColorBrush(Color.FromArgb(0xFF, 0x3F, 0x51, 0xB5));
+    }
+
+    private void btnRoot_MouseEnter(object sender, MouseEventArgs e)
+    {
+      btnRoot.Background = new SolidColorBrush(Color.FromArgb(0xFF, 0x5C, 0x6b, 0xC0));
+    }
+
+    private void btnRoot_MouseLeave(object sender, MouseEventArgs e)
+    {
+      btnRoot.Background = new SolidColorBrush(Color.FromArgb(0xFF, 0x3F, 0x51, 0xB5));
+    }
+
+    //===== Eventy spoštějící se při najetí a opuštění na tlačítka mocniny =====
+    private void grdPowBtn_MouseEnter(object sender, MouseEventArgs e)
+    {
+      btnPow.Background = new SolidColorBrush(Color.FromArgb(0xFF, 0x5C, 0x6b, 0xC0));
+    }
+
+    private void grdPowBtn_MouseLeave(object sender, MouseEventArgs e)
+    {
+      btnPow.Background = new SolidColorBrush(Color.FromArgb(0xFF, 0x3F, 0x51, 0xB5));
+    }
+
+    private void btnPow_MouseEnter(object sender, MouseEventArgs e)
+    {
+      btnPow.Background = new SolidColorBrush(Color.FromArgb(0xFF, 0x5C, 0x6b, 0xC0));
+    }
+
+    private void btnPow_MouseLeave(object sender, MouseEventArgs e)
+    {
+      btnPow.Background = new SolidColorBrush(Color.FromArgb(0xFF, 0x3F, 0x51, 0xB5));
     }
   }
 }
