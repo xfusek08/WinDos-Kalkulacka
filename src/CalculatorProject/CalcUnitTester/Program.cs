3ef2f8eb7b969ab8b4a87a519a0afa628d3e73e1
Pavel Vosyka <xvosyk00@stud.fit.vutbr.cz>
2017-04-08 22:34:51 +0200

    Úprava testovacího programu
    Testovací program pro Calculation.cs byl upraven, nyní vypisuje jak přímo hodnotu výsledku, tak řetězce z metody GetAsString() ve všech dostupných soustavách.

--- src/CalculatorProject/CalcUnitTester/Program.cs
+++ src/CalculatorProject/CalcUnitTester/Program.cs
@@ -18,6 +18,10 @@ namespace CalcUnitParser
 
         Calculation calculation = new Calculation(expr);
         Console.WriteLine("Result: {0}", calculation.Value);
+        Console.WriteLine("GetAsString <Dec>: {0}", calculation.GetAsString(NumSystem.Dec, ""));
+        Console.WriteLine("GetAsString <Bin>: {0}", calculation.GetAsString(NumSystem.Bin, ""));
+        Console.WriteLine("GetAsString <Oct>: {0}", calculation.GetAsString(NumSystem.Oct, ""));
+        Console.WriteLine("GetAsString <Hex>: {0}", calculation.GetAsString(NumSystem.Hex, ""));
       } while (true);
     }
   }
