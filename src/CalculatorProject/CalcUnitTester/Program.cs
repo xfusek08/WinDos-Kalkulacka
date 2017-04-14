4ce00cfa660c4e3372da02bb8f9e1cc9f1ab18db
Pavel Vosyka <xvosyk00@stud.fit.vutbr.cz>
2017-04-14 21:19:29 +0200

    Úprava testovacího programu výpočetní jednotky
    -implementace volby zadávání v jiných soustavách
    -program již nepoužívá calculation.GetAsString() z důvodu zastaralosti této metody.

--- src/CalculatorProject/CalcUnitTester/Program.cs
+++ src/CalculatorProject/CalcUnitTester/Program.cs
@@ -9,19 +9,49 @@ namespace CalcUnitParser
 {
   class Program
   {
+    //program pro interní testování výpočetní jednotky při vývoji
+    //po spustení program vyžaduje zadání číselné soustavy vstupu
     static void Main(string[] args)
     {
+      Console.WriteLine("Pozor při zadávání v jiné soustavě než desítkové, nejsou podporovány výrazy, pouze jednotlivá čísla!");
+      Console.Write("Enter input number system [2/8/10/16]: ");
+      int numsys;
+      NumSystem sys;
+      int.TryParse(Console.ReadLine(), out numsys);
+      switch (numsys)
+      {
+        case 2:
+          sys = NumSystem.Bin;
+          break;
+        case 8:
+          sys = NumSystem.Oct;
+          break;
+        case 10:
+          sys = NumSystem.Dec;
+          break;
+        case 16:
+          sys = NumSystem.Hex;
+          break;
+        default:
+          sys = NumSystem.Dec;
+          break;
+      }
+      Console.WriteLine("Zvolena soustava {0}", sys.ToString());
       do
       {
-        Console.Write("Expresion: ");
+        Console.Write("Enter expresion: ");
         string expr = Console.ReadLine();
+        if(sys != NumSystem.Dec)
+        {
+          expr = NumberConverter.ToDouble(expr, sys).ToString(System.Globalization.CultureInfo.InvariantCulture);
+        }
 
         Calculation calculation = new Calculation(expr);
         Console.WriteLine("Result: {0}", calculation.Value);
-        Console.WriteLine("GetAsString <Dec>: {0}", calculation.GetAsString(NumSystem.Dec, ""));
-        Console.WriteLine("GetAsString <Bin>: {0}", calculation.GetAsString(NumSystem.Bin, ""));
-        Console.WriteLine("GetAsString <Oct>: {0}", calculation.GetAsString(NumSystem.Oct, ""));
-        Console.WriteLine("GetAsString <Hex>: {0}", calculation.GetAsString(NumSystem.Hex, ""));
+        Console.WriteLine("GetAsString <Dec>: {0}", NumberConverter.ToString(calculation.Value, (int)NumSystem.Dec, ""));
+        Console.WriteLine("GetAsString <Bin>: {0}", NumberConverter.ToString(calculation.Value, (int)NumSystem.Bin, ""));
+        Console.WriteLine("GetAsString <Oct>: {0}", NumberConverter.ToString(calculation.Value, (int)NumSystem.Oct, ""));
+        Console.WriteLine("GetAsString <Hex>: {0}", NumberConverter.ToString(calculation.Value, (int)NumSystem.Hex, ""));
       } while (true);
     }
   }
